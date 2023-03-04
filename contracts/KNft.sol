37d0fb800f1ded6af1d09122ce173408bc5a0d40
truecode112 <truecoder112@gmail.com>
2023-03-05 01:14:47 +0900

    init project

---
+++ contracts/KNft.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+import { Base64 } from "./libraries/Base64.sol";
+
+contract KNft is ERC721URIStorage{
+  using Counters for Counters.Counter;
+  Counters.Counter private _tokenId;
+
+  string public collectionName;
+  string public collectionSymbol;
+
+  constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) {}
+
+  function createNft() public returns(uint256) {
+    _tokenId.increment();
+    uint256 newItemId = _tokenId.current();
+
+    string memory baseSvg = "<svg viewBox='0 0 350 350' xmlns='http://www.w3.org/2000/svg' ><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><circle cx='175' cy='175' r='175' width='100%' height='100%' fill='black'></circle><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>Token #";
+
+    string memory finalSvg = string(abi.encodePacked(baseSvg, Strings.toString(newItemId), "</text></svg>"));
+
+    string memory json = Base64.encode(
+        bytes(
+            string(
+                abi.encodePacked(
+                '{"name": "',
+                    Strings.toString(newItemId),
+                    '", "description": "A highly acclaimed collection Warriors", "image": "data:image/svg+xml;base64,',
+                    Base64.encode(bytes(finalSvg)),
+                '"}'
+                )
+            )
+        )
+    );
+
+    string memory finalTokenURI = string(abi.encodePacked(
+        "data:application/json;base64,", json
+    ));
+
+    _safeMint(msg.sender, newItemId);
+    _setTokenURI(newItemId, finalTokenURI);
+
+
+    return newItemId;
+  }
+}
